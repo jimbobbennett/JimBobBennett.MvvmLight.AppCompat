e14cb3c167f88da620ffb508612dd78da942c4b7
Jim Bennett <jimbobbennett@mac.com>
2016-02-08 12:43:37 +1300

    Initial checkin

---
+++ MvvmLight.AppCompat/AppCompatActivityBase.cs
@@ -0,0 +1,30 @@
+﻿using Android.Support.V7.App;
+
+namespace JimBobBennett.MvvmLight.AppCompat
+{
+    public abstract class AppCompatActivityBase : AppCompatActivity
+    {
+        public static AppCompatActivityBase CurrentActivity { get; private set; }
+
+        internal string ActivityKey { get; private set; }
+
+        internal static string NextPageKey { get; set; }
+        
+        public static void GoBack()
+        {
+            CurrentActivity?.OnBackPressed();
+        }
+        
+        protected override void OnResume()
+        {
+            CurrentActivity = this;
+            if (string.IsNullOrEmpty(ActivityKey))
+            {
+                ActivityKey = NextPageKey;
+                NextPageKey = null;
+            }
+            base.OnResume();
+        }
+    }
+}
+
